bafd07801c7dff763f0b9af216a6701f1c96d088
Aaron Bickle <aaronbickle388@hotmail.com>
2019-03-02 12:14:50 -0800

    Removing uneeded package references; versions were borking the environment Removing role provider configs, as we are not handling security Updating configs with proper unicorn version Serializing update tool into source control

---
+++ src/ProductInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("XC-Hackers")]
+[assembly: AssemblyProduct("XC-Hackers 2019 Hackathon")]
+[assembly: AssemblyCopyright("Copyright © 2019 XC-Hackers")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("9.1.0.*")]
